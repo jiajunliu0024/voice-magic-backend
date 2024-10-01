49ae5db5f13465c18a4f5e00b8c5dfb48a2da1ca
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-10-01 21:33:29 +1000

    remove the pwd

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const certFile = fs.readFileSync("./ca.pem").toString();
 const client = new pg.Client({
   user: "avnadmin", // Replace 'your_username' with the username for your PostgreSQL user
   host: "pg-magic-voice-jiajunliu0024-3a27.j.aivencloud.com", // Replace 'your_host' with the host for your PostgreSQL server
-  password: "AVNS_1OqEK9Eti-yyJzm8kmX", // Replace 'your_password' with the password for your PostgreSQL user
+  password: process.env.DB_PASSWORD, // Replace 'your_password' with the password for your PostgreSQL user
   port: 25720, // The default port for PostgreSQL is 5432
   database: "dev_voice_magic", // Replace 'your_database' with the name of your PostgreSQL database
   ssl: {
